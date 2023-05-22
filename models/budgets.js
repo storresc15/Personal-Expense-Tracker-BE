3f8e8fc31bb4a44065efa086fb76698991cc7c11
Sebastian Torres <sebastiantorres@Sebastians-MacBook-Pro.local>
2023-05-22 13:04:51 -0600

    Initial commit

---
+++ models/budgets.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const budgetsSchema = new Schema({
+  amount: {
+    type: Number,
+    required: true
+  },
+  description: String,
+  category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  startDate: Date,
+  endDate: Date
+});
+
+const Budget = mongoose.model('Budget', budgetsSchema);
+module.exports = Budget;
---
+++ models/categories.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const categorySchema = new Schema({
+  amount: String,
+  description: String,
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  type: String //Not sure on the use of the type of category -- do we have a business process defined for this?
+});
+
+const Category = mongoose.model('Category', categorySchema);
+module.exports = Category;
