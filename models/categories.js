bea2d0fb0285a986d4005c935b9c0f3246843171
Sebastian Torres <sebastiantorres@Sebastians-MacBook-Pro.local>
2023-06-12 17:15:54 -0600

    Husky test

--- models/categories.js
+++ models/categories.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const categorySchema = new Schema({
   amount: String,
   description: String,
-  type: String,//Not sure on the use of the type of category -- do we have a business process defined for this?
+  type: String,//Not sure on the use of the type of category -- do we have a business process defined for this??
   user: { 
     type: Schema.Types.ObjectId, 
     ref: 'User' 
