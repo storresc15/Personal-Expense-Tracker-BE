0bdd0958e8b1f34bebc2514fe35722c9834454e8
Sebastian Torres <sebastiantorres@Sebastians-MacBook-Pro.local>
2023-06-19 12:06:57 -0600

    Updated app.js and models index for testing database connection

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const url = process.env.MONGO_DB_CONNECTION_STRING
 
 mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
     .then(() => {
-        console.log("MONGO CONNECTION OPEN!!!")
+        console.log("MONGO CONNECTION OPEN FOR EXPENSES!!!")
     })
     .catch(err => {
         console.log("OH NO MONGO CONNECTION ERROR!!!!")
@@ -11,7 +11,7 @@ mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedT
     })
 
 //The below exports from models represent the domains of the data
-module.exports.User = require('./user');
+module.exports.User = require('./users');
 module.exports.Budget = require('./budgets');
 module.exports.Category = require('./categories');
 module.exports.Expense = require('./expenses');
