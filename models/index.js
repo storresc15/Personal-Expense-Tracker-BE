2d24310f3ce183c6529c31d84cea4e68777d2ffe
Sebastian Torres <sebastiantorres@Sebastians-MacBook-Pro.local>
2023-06-20 12:41:01 -0600

    updated index.js for models

--- models/index.js
+++ models/index.js
@@ -1,16 +1,21 @@
-const mongoose = require("mongoose");
-const url = process.env.MONGO_DB_CONNECTION_STRING
+const mongoose = require('mongoose');
+const url = process.env.MONGO_DB_CONNECTION_STRING;
 
-mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("MONGO CONNECTION OPEN FOR EXPENSES!!!")
-    })
-    .catch(err => {
-        console.log("OH NO MONGO CONNECTION ERROR!!!!")
-        console.log(err)
-    })
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('MONGO CONNECTION OPEN FOR EXPENSES!!!');
+  })
+  .catch((err) => {
+    console.log('OH NO MONGO CONNECTION ERROR!!!!');
+    console.log(err);
+  });
 
-//The below exports from models represent the domains of the data
+//The below exports from models represent the domains of the data, since only needed to use index as import
 module.exports.User = require('./users');
 module.exports.Budget = require('./budgets');
 module.exports.Category = require('./categories');
